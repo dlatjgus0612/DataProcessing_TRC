60a7c99c308c2962f26a455a82d989937f71ef43
dlatjgus0612 <dlatjgus0612@gmail.com>
2024-11-05 12:10:46 +0900

    feat/add03histogram

--- 01chap/02_numpy_matplotlib.py
+++ 01chap/02_numpy_matplotlib.py
@@ -1,7 +1,3 @@
-# date : 2024.5.16, 북서태평양 해상에서
-# made by : Peter
-# file_name : matplotlib_test-1.py
-
 from matplotlib import pyplot as plt
 import numpy as np 
 
@@ -10,43 +6,66 @@ def draw_graph():
     y = x * 3
 
     # 3가지 산점도 
-    plt.scatter(x, y, color='blue', marker = '+', s = 100) # s = 마커 크기
+    plt.scatter(x, y, color='blue', marker='+', s=100)  # s = 마커 크기
     plt.plot(x, y-2, linestyle=":", 
-            color = 'blue', 
-            marker = 'o',
-            markeredgecolor = 'g',
-            markerfacecolor = 'r'
-            )
+             color='blue', 
+             marker='o',
+             markeredgecolor='g',
+             markerfacecolor='r'
+             )
     plt.plot(x, y-5, linestyle="--", 
-            color = 'black', 
-            marker = 's',
-            markeredgecolor = 'm',
-            markerfacecolor = 'c'
-            )
+             color='black', 
+             marker='s',
+             markeredgecolor='m',
+             markerfacecolor='c'
+             )
 
     plt.show()
 
 def color_graph():
-    x = np.arange(1, 10)
-    y = x * 3
+    cmaps = plt.colormaps()  # Matplotlib에서 지원하는 모든 색상표 가져오기
+    for cmap in cmaps:       # 리스트를 반복
+        print(cmap)
+        
+    data = np.random.rand(10, 10)  # 예시 데이터 생성
+    
+    plt.imshow(data, cmap='viridis')  # 출력
+    plt.colorbar()
+    plt.title('Example with Viridis Colormap')
+    plt.show()
 
-    # 3가지 산점도 
-    plt.scatter(x, y, color='blue', marker = '+', s = 100) # s = 마커 크기
-    plt.plot(x, y-2, linestyle=":", 
-            color = 'blue', 
-            marker = 'o',
-            markeredgecolor = 'g',
-            markerfacecolor = 'r'
-            )
-    plt.plot(x, y-5, linestyle="--", 
-            color = 'black', 
-            marker = 's',
-            markeredgecolor = 'm',
-            markerfacecolor = 'c'
-            )
+def sub_graph():
+    x1 = np.linspace(0.0, 5.0)
+    x2 = np.linspace(0.0, 2.0)
 
+    y1 = np.cos(2 * np.pi * x1) * np.exp(-x1)
+    y2 = np.cos(2 * np.pi * x2)
+
+    ax1 = plt.subplot(2, 1, 1)
+    plt.plot(x1, y1, 'o-')
+    plt.title('1st Graph')
+    plt.ylabel('Damped oscillation')
+
+    #plt.xticks(visible = False) # Hide x-ticks and labels
+    ax2 = plt.subplot(2, 1, 2, sharex=ax1)  # Sharing x-axis with ax1
+    plt.plot(x2, y2, '.-')
+    plt.title('2nd Graph')
+    plt.xlabel('time (s)')
+    plt.ylabel('undamped')
+    plt.tight_layout()  # Adjust layout to prevent overlap
+    plt.show()
+    
+def grid_graph():
+    plt.figure(figsize=(10, 12)) 
+    ax1 = plt.subplot2grid((3,3), (0,0), colspan=3)   
+    ax2 = plt.subplot2grid((3,3), (1,0), colspan=2)   
+    ax3 = plt.subplot2grid((3,3), (1,2), rowspan=2)   
+    ax4 = plt.subplot2grid((3,3), (2,0))   
+    ax5 = plt.subplot2grid((3,3), (2,1))   
     plt.show()
 
 
 draw_graph()
 color_graph()
+sub_graph()
+grid_graph()
