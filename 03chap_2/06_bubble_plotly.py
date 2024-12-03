036c6454c7c7de177483c8b53cd5162dacc13a7f
dlatjgus0612 <dlatjgus0612@gmail.com>
2024-12-03 18:00:20 +0900

    Feat : Adding codes ... (not yet)
    - 03chap_2/06_kma_folium.py (modifying)
    - #4

--- 03chap_2/06_bubble_plotly.py
+++ 03chap_2/06_bubble_plotly.py
@@ -1,2 +1,14 @@
 import plotly.graph_objects as go
 import pandas as pd
+
+def setpath():
+    df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_us_cities.csv')
+
+def createmap():
+    pass
+
+def ploton():
+    pass
+
+if __name__ == "__main__":
+    main()
--- 03chap_2/06_kma_folium.py
+++ 03chap_2/06_kma_folium.py
@@ -1,3 +1,23 @@
 import pandas as pd
 import folium
 import os
+
+def setpath():
+    work_path = '/home/ish/DataProcessing_TRC/data/'
+    icon_path = os.path.join(work_path, 'weather_station_icon.png')
+    data_path = os.path.join(work_path, 'kma_sun_st.csv')
+    df = pd.read_csv(data_path, names=['station', 'lat', 'lon'],
+                    encoding='euc-kr')
+    outfile = os.path.join(work_path, 'my_folium_map.html')
+    return outfile, df
+
+def createmap():
+    map_center = [36, 128]
+    map = folium.Map(location=map_center, zoom_start=7)
+    return map
+
+def ploton(df):
+    for index, row in df.iterrows():
+
+if __name__ == "__main__":
+    main()
